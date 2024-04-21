4851915ada364b105cca000a6e9a78631369b5f5
MohanBhimavarapu <bhimavarapumohansai@gmail.com>
2024-04-22 00:06:05 +0530

    special changes

--- repo/shell-script/special_variables.sh
+++ repo/shell-script/special_variables.sh
@@ -2,3 +2,9 @@
 
 echo "This will print all the variables : $@"
 echo "This will print the script name : $0"
+echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
