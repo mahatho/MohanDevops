18089afb3948e3574e99e9620ae54a55c52b79b2
MohanBhimavarapu <bhimavarapumohansai@gmail.com>
2024-04-23 19:55:01 +0530

    function changes

--- repo/shell-script/practice1.sh
+++ repo/shell-script/practice1.sh
@@ -8,3 +8,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
