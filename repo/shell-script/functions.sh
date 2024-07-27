2173d660aa50d632567258b83d8e6fa3358d583c
Mohan Bhimavarapu <bhimavarapumohansai@gmail.com>
2024-07-27 19:19:58 +0530

    shell changes

--- repo/shell-script/functions.sh
+++ repo/shell-script/functions.sh
@@ -4,23 +4,27 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[30m"
+G="\e[31m"
+Y="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo "$2...FAILURE"
+        echo "$2...FAILURE$R"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo "$2...SUCCESS$G"
     fi
 }
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
+    echo "Please run this script with root access.$Y"
     exit 1 # manually exit if error comes.
 else
-    echo "You are super user."
+    echo "You are super user.$G"
 fi
 
 dnf install mysql -y &>>$LOGFILE
