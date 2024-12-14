8fd4ba40928c73e6d2c7b42784b2832e8a664a82
Mohan Bhimavarapu <bhimavarapumohansai@gmail.com>
2024-12-14 15:26:23 +0530

    new changes

--- repo/shell-script/ifelse.sh
+++ repo/shell-script/ifelse.sh
@@ -6,7 +6,7 @@ echo "Please enter the number : $Number"
 
 if [ $Number -gt 100 ]
 then
-    echo "Number is greater than 100"
+    echo "$Number is greater than 100"
 else
-    echo "Number is not greater than 100"
+    echo "$Number is not greater than 100"
 fi
