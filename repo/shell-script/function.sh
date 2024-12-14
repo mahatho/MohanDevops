1987d61cc42eb8769460a5c8bab46c89a1a31776
Mohan Bhimavarapu <bhimavarapumohansai@gmail.com>
2024-12-14 15:59:21 +0530

    new changes

--- repo/shell-script/function.sh
+++ repo/shell-script/function.sh
@@ -3,8 +3,11 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "Exit status : $1"
-    echo "What we are doing? : $2"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.....Failure"
+        exit 1
+    fi
 }
 
 if [ $USERID -ne 0 ]
