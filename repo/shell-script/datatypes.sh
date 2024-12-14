e27556eaf8f59ae3528482ef72d065dae0459f28
Mohan Bhimavarapu <bhimavarapumohansai@gmail.com>
2024-12-14 14:59:19 +0530

    new changes

--- repo/shell-script/datatypes.sh
+++ repo/shell-script/datatypes.sh
@@ -2,7 +2,6 @@
 
 NO1=$1
 NO2=$2
-
 SUM=$(($NO1+$NO2))
 
-echo "Here we are doing the Summation :- So the Sum value of $NO1 and $NO2 is :- $SUM "
+echo "So the Sum value of $NO1 and $NO2 is :- $SUM "
